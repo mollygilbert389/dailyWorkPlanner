f4a7d67b4829ee82ae73cd5d55415a6515016eb9
Molly Gilbert <msgilbert389@gmail.com>
2020-05-25 13:49:02 -0500

    adding updates to editable text

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ $(document).ready(function(){
     let currentDayofWeek = moment().format("[Today is] dddd")
     let currentTime = moment().format('h:mm a')
 
-    console.log(currentTime)
-
     let dailyPlannerTimes = ['08:00 am', '09:00 am', '10:00 am', '11:00 am', "12:00 pm", '1:00 pm', '2:00pm', '3:00pm', '4:00 pm', '5:00pm']
 
     for (i=0;i<dailyPlannerTimes.length;i++) {
@@ -11,10 +9,14 @@ $(document).ready(function(){
             newDailyPlannerSpace = $('<div>')
             outerBox = $('<div>')
             meetingTextSpace = $('<div>')
+            newlistCreate = $('<ul>')
+            newlistCreate.addClass('listSpace')
+            meetingTextSpace.append(newlistCreate)
             newTimes = $('<input>')
             newTimes.attr('placeholder', 'Add Plans here')
             meetingTextSpace.addClass('meetingInfoSpace')
             outerBox.addClass('redBufferSpace')
+            outerBox.attr('value', dailyPlannerTimes[i])
             outerBox.append(dailyPlannerTimes[i])
             outerBox.append(meetingTextSpace)
             outerBox.append(newTimes)
@@ -22,19 +24,14 @@ $(document).ready(function(){
             newDailyPlannerSpace.append(outerBox)
             $('#meetingSpaces').append(newDailyPlannerSpace)
 
-            let currentHour = moment().format('h:00 a')
-            console.log(outerBox)
-            
-
-            // if (currentHour == dailyPlannerTimes[i]) {
-            //     console.log(currentHour)
-            //     outerBox.attr('class', 'yellowBufferSpace')
-            // }
             
         } else {
             newDailyPlannerSpace = $('<div>')
             outerBox = $('<div>')
             meetingTextSpace = $('<div>')
+            newlistCreate = $('<ul>')
+            newlistCreate.addClass('listSpace')
+            meetingTextSpace.append(newlistCreate)
             newTimes = $('<input>')
             newTimes.attr('placeholder', 'Add Plans here')
             meetingTextSpace.addClass('meetingInfoSpace')
@@ -48,19 +45,27 @@ $(document).ready(function(){
             }
     }
 
+    let yellowBox = $('.redBufferSpace').slice(-1)[0]
+        if (currentTime < '5:59 pm') {
+            yellowBox.setAttribute('class','yellowBufferSpace')
+        } 
 
     $('.hourSpace').keypress(function() {
         if ( event.which == 13 ) {
-            let targetText = $(this).val()
-            console.log(targetText)
-            let parentDiv = $(this).parent()
-            let neededDiv = parentDiv.children()
-            neededDiv = neededDiv[0]
-            neededDiv.append(targetText)
+            targetText = $(this).val()
+            parentDiv = $(this).parent()
+            neededDiv = parentDiv.children()
+            targetDiv = neededDiv[0].firstChild
+            let newListItem = document.createElement('li')
+            newListItem.setAttribute('class', 'newListItem')
+            newListItem.append(targetText)
+            targetDiv.append(newListItem)
+            $(this).val('')
         }
     })
 
-
-
+    $('.meetingInfoSpace').on('click', '.newListItem', function(){
+        $(this).text('')
+    })
 
 })
